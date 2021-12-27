6443c28d10cf6d935e68b4beac0049fcb0c06fbd
ponttor <alex@heimstudio.ru>
2021-12-27 21:34:47 +0100

    nav border

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
   const { isAuthenticated, logout } = useContext(AuthContext);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
         <div className="navbar-brand">{i18next.t('title')}</div>
         {isAuthenticated && <button onClick={logout} className="btn btn-primary" type="button">{i18next.t('logOutButton')}</button>}
